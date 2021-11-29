1a2f09c572b372c72697076325fc60a53a4bdeeb
ember2000 <zshan@email.wm.edu>
2021-11-29 16:33:33 -0500

    changed the code so the code only deals with databases

--- get_artists_names.py
+++ get_artists_names.py
@@ -1,10 +1,8 @@
 #############
 # Name: Ember Shan
 # This file is to get a list of 200 artists names from the website
-# and store it in a txt file
-# Then create the Artists table in the database 
-# based on the txt file scraped from the website
-# a total of 200 artist names so a total of 10 times to add all items
+# And store them in the Artists table in the database 
+# a total of 200 artist names so a total of 8 times executing this code to add all items
 #############
 
 import os 
@@ -21,68 +19,72 @@ def createSoup():
     return soup
 
 
-def getData(soup):
-    names = []
-    table = soup.find('table')
-    rows = table.find_all("tr")
-    for r in rows[1:]:
-        name_cols = r.find('td', class_ = "name")
-        names.append(name_cols.text.lstrip('\n'))
-
-    dir_path = os.path.dirname(os.path.realpath(__file__)) 
-    path = dir_path + '/' + 'artist_names.txt'
-    f = open(path, 'w')
-    f.writelines(names[0:200])
-    f.close()
+def createSoupFromFile():
+    # in case the website changes or denied web scraping
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    path = dir_path + '/' + 'Most_Popular_Artists_Worldwide.html'
+    with open(path) as pg:
+        soup = BeautifulSoup(pg, "html.parser")
+    return soup
 
 
-def getDataFromFile():
-    dir_path = os.path.dirname(os.path.realpath(__file__)) 
-    path = dir_path + '/' + 'artist_names.txt'
-    # get the names in the file
-    with open(path, 'r') as file: 
-        names = file.readlines()
-    # return the list of names in order 
-    return names
-
-
-def createArtistDatabase(cur, conn):
+def getLastID(cur):
     cur.execute('CREATE TABLE IF NOT EXISTS Artists (artist_id INTEGER PRIMARY KEY, name TEXT)')
-    names = getDataFromFile()
     # checking what have already been stored to the database
     # get the last row of the database
     cur.execute('SELECT artist_id, name FROM Artists ORDER BY artist_id DESC LIMIT 1')
     row = cur.fetchone()
-    # check if this is the first time inserting the data
+    # check if this is the first time inserting any data
     if row: 
-        print(row)
         # get the id of the last inserted item 
         count = row[0]
+        if count == 200: print('All 200 items already inserted')
     else: 
-        count = 1 # count for id
-    # insert only 20 items to the database
-    end = count + 20 if len(names) >= count + 20 else len(names)
-    for i in range(count-1, end): 
+        print('first time inserting the data')
+        count = 0 # count for id
+    return count
+
+
+def getData(soup, cur, conn):
+    names = []
+    start = getLastID(cur)
+
+    table = soup.find('table')
+    # check if the data from website is none
+    if table != None:
+        rows = table.find_all("tr")
+        # scraping only 25 elements from the website
+        for r in rows[start + 1: start + 26]:
+            name_cols = r.find('td', class_ = "name")
+            names.append(name_cols.text.strip('\n'))
+
+    # create the artist database 
+    createArtistDatabase(cur, conn, names, start)
+
+
+def createArtistDatabase(cur, conn, names, start):
+    # iteraing through the scraped data and insert them into the database
+    for i in range(0, len(names)): 
         cur.execute('INSERT OR IGNORE INTO Artists (artist_id, name) VALUES (?, ?)', \
-            (count, names[i].strip('\n')))
-        count += 1
+            (start+1, names[i].strip('\n')))
+        start += 1
     conn.commit()
 
 
 def main():
-    # scrap data from the website
-    dir_path = os.path.dirname(os.path.realpath(__file__)) 
-    path = dir_path + '/' + 'artist_names.txt'
-    # checking if the file is empty
-    if os.stat(path).st_size == 0:
-        print("file is empty, getting the data from website")
-        s = createSoup()
-        getData(s)
-    # create the artists database
+    # create the soup
+    s = createSoup()
+    # if cannot scrape from the website, then scrape it form the file
+    # s = createSoupFromFile()
+
+    # create the database
+    dir_path = os.path.dirname(os.path.realpath(__file__))
     conn = sqlite3.connect(dir_path + '/' + "finalproject.db")
     cur = conn.cursor()
-    createArtistDatabase(cur, conn)
 
+    # get the needed data from the website and create the artists database
+    getData(s, cur, conn)
     
+
 if __name__ == "__main__":
     main()
